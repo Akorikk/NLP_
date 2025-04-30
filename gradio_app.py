f075b6499ba1a6b768e51eb4ad968f52b42e5fd4
Abhishek Kori <abhishekkori601@gmail.com>
2025-04-30 15:14:25 +0530

    gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -1,5 +1,22 @@
+import gradio as gr
+
 def main():
-    print("hellow hi ")
+    with gr.Blocks() as iface:
+        with gr.Row():
+            with gr.Column():
+                gr.HTML("<h1>Classification (Zero short Classifiers)</h1>")
+                with gr.Row():
+                    with gr.Column():
+                        plot = gr.BarPlot()  
+                    with gr.Column():
+                        them_list = gr.Textbox(label="Themes")
+                        subtitles_path = gr.Textbox(label= "subtitles or script Path")
+                        save_path = gr.Textbox(label="Save Path")
+                        get_theme_button = gr.Button("Get Themes")
+
+
+    iface.launch(share=True)                   
+
 
 if __name__ == "__main__":
     main()    
